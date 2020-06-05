c7eb9258f844fa411a8e6afd43c7cc82c16744cf
Sergey Tovt <st@norse.co>
2020-06-05 15:40:18 +0300

    new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import {
+  Container,
   Box,
   TextField,
   Chip,
@@ -10,25 +11,34 @@ import {
   TableHead,
   TableRow,
   TableCell,
-  TableBody
+  TableBody,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem
 } from '@material-ui/core';
 import Autocomplete, {
   AutocompleteChangeReason,
   AutocompleteChangeDetails
 } from '@material-ui/lab/Autocomplete';
 
-const ROLES = ['Мафія', 'Терорист', 'Мирний', 'Шлюха', 'Шериф', 'Лікар', 'Телохранітель'];
+const MAFIA = 'Мафія';
+const DOCTOR = 'Лікар';
+const SHERIFF = 'Шериф';
+const BITCH = 'Шлюха';
+const TERRORIST = 'Терорист';
+const SECURITY = 'Телохранітель';
+const PIECE = 'Мирний';
+
+const ROLES = [MAFIA, DOCTOR, SHERIFF, BITCH, TERRORIST, SECURITY, PIECE];
 const INITIAL_VALUE = [
-  'Мафія',
-  'Мафія',
-  'Терорист',
-  'Шлюха',
-  'Шериф',
-  'Лікар',
-  'Мирний',
-  'Мирний',
-  'Мирний',
-  'Мирний'  
+  ...Array(2).fill(MAFIA),
+  DOCTOR,
+  SHERIFF,
+  BITCH,
+  TERRORIST,
+  SECURITY,
+  ...Array(3).fill(PIECE)
 ];
 
 const shuffleArray = (arr: any[]) =>
@@ -39,6 +49,15 @@ const shuffleArray = (arr: any[]) =>
 
 const App = () => {
   const [value, setValue] = useState(INITIAL_VALUE);
+
+  const [chooses, setChooses] = useState({
+    bitch: '',
+    mafia: '',
+    doctor: '',
+    security: '',
+    sheriff: ''
+  });
+
   const [shuffledValue, setShuffledValue] = useState<string[]>([]);
 
   const handleOnChange = useCallback(
@@ -72,58 +91,214 @@ const App = () => {
 
   const handleOnGenerate = useCallback(() => {
     const shuffled = shuffleArray(value);
+
     setShuffledValue(shuffled);
+
+    setChooses({
+      bitch: '',
+      mafia: '',
+      doctor: '',
+      security: '',
+      sheriff: ''
+    });
   }, [value]);
 
+  const handleOnChangeChoose = useCallback(
+    (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+      const { name } = event.target;
+
+      if (name) {
+        setChooses(values => ({
+          ...values,
+          [name]: event.target.value
+        }));
+      }
+    },
+    []
+  );
+
   return (
-    <Box my={5} px={3} mx='auto' maxWidth={600}>
-      <Autocomplete
-        multiple
-        freeSolo
-        options={ROLES}
-        getOptionLabel={option => option}
-        getOptionSelected={() => false}
-        renderTags={
-          (val, getTagProps) =>
-            val.map((option, index) => (
-              <Chip variant='outlined' label={option} {...getTagProps({ index })} />
-            ))
-          // eslint-disable-next-line react/jsx-curly-newline
-        }
-        renderInput={params => (
-          <TextField {...params} variant='outlined' label='Ролі' placeholder='Нова роль' />
+    <Container component='main' maxWidth='sm'>
+      <Box py={{ xs: 2, sm: 3 }}>
+        <Box mb={4}>
+          <Autocomplete
+            multiple
+            freeSolo
+            options={ROLES}
+            getOptionLabel={option => option}
+            getOptionSelected={() => false}
+            renderTags={
+              (val, getTagProps) =>
+                val.map((option, index) => (
+                  <Chip variant='outlined' label={option} {...getTagProps({ index })} />
+                ))
+              // eslint-disable-next-line react/jsx-curly-newline
+            }
+            renderInput={params => (
+              <TextField {...params} variant='outlined' label='Ролі' placeholder='Нова роль' />
+            )}
+            onChange={handleOnChange}
+            value={value}
+          />
+        </Box>
+
+        <Box textAlign='center' mb={4}>
+          <Button color='primary' variant='contained' onClick={handleOnGenerate}>
+            Згенерувати
+          </Button>
+        </Box>
+
+        {!!shuffledValue.length && (
+          <Box>
+            <Box mb={4}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>№</TableCell>
+                      <TableCell>Роль</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {shuffledValue.map((row, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{index + 1}</TableCell>
+                        <TableCell>{row}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+
+            {shuffledValue.includes(BITCH) && (
+              <Box mb={3}>
+                <FormControl fullWidth variant='outlined'>
+                  <InputLabel htmlFor='bitch'>Вибір шлюхи</InputLabel>
+                  <Select
+                    label='Вибір шлюхи'
+                    name='bitch'
+                    value={chooses.bitch}
+                    onChange={handleOnChangeChoose}
+                  >
+                    <MenuItem value=''>
+                      <em>Ніякий</em>
+                    </MenuItem>
+                    {shuffledValue.map((val, i) =>
+                      val === BITCH ? null : (
+                        <MenuItem value={i} key={i}>
+                          {i + 1} - {val}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+
+            {shuffledValue.includes(MAFIA) && (
+              <Box mb={3}>
+                <FormControl fullWidth variant='outlined'>
+                  <InputLabel htmlFor='mafia'>Вибір мафії</InputLabel>
+                  <Select
+                    label='Вибір мафії'
+                    name='mafia'
+                    value={chooses.mafia}
+                    onChange={handleOnChangeChoose}
+                  >
+                    <MenuItem value=''>
+                      <em>Ніякий</em>
+                    </MenuItem>
+                    {shuffledValue.map((val, i) =>
+                      val === MAFIA || val === TERRORIST ? null : (
+                        <MenuItem value={i} key={i}>
+                          {i + 1} - {val}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+
+            {shuffledValue.includes(DOCTOR) && (
+              <Box mb={3}>
+                <FormControl fullWidth variant='outlined'>
+                  <InputLabel htmlFor='doctor'>Вибір лікаря</InputLabel>
+                  <Select
+                    label='Вибір лікаря'
+                    name='doctor'
+                    value={chooses.doctor}
+                    onChange={handleOnChangeChoose}
+                  >
+                    <MenuItem value=''>
+                      <em>Ніякий</em>
+                    </MenuItem>
+                    {shuffledValue.map((val, i) =>
+                      val === DOCTOR ? null : (
+                        <MenuItem value={i} key={i}>
+                          {i + 1} - {val}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+
+            {shuffledValue.includes(SECURITY) && (
+              <Box mb={3}>
+                <FormControl fullWidth variant='outlined'>
+                  <InputLabel htmlFor='security'>Вибір телохранітеля</InputLabel>
+                  <Select
+                    label='Вибір телохранітеля'
+                    name='security'
+                    value={chooses.security}
+                    onChange={handleOnChangeChoose}
+                  >
+                    <MenuItem value=''>
+                      <em>Ніякий</em>
+                    </MenuItem>
+                    {shuffledValue.map((val, i) =>
+                      val === SECURITY ? null : (
+                        <MenuItem value={i} key={i}>
+                          {i + 1} - {val}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+
+            {shuffledValue.includes(SHERIFF) && (
+              <Box mb={3}>
+                <FormControl fullWidth variant='outlined'>
+                  <InputLabel htmlFor='sheriff'>Вибір шерифа</InputLabel>
+                  <Select
+                    label='Вибір шерифа'
+                    name='sheriff'
+                    value={chooses.sheriff}
+                    onChange={handleOnChangeChoose}
+                  >
+                    <MenuItem value=''>
+                      <em>Ніякий</em>
+                    </MenuItem>
+                    {shuffledValue.map((val, i) =>
+                      val === SHERIFF ? null : (
+                        <MenuItem value={i} key={i}>
+                          {i + 1} - {val}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+          </Box>
         )}
-        onChange={handleOnChange}
-        value={value}
-      />
-
-      <Box textAlign='center' my={3}>
-        <Button color='primary' variant='contained' onClick={handleOnGenerate}>
-          Згенерувати
-        </Button>
       </Box>
-
-      {!!shuffledValue.length && (
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>№</TableCell>
-                <TableCell>Роль</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {shuffledValue.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{row}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
-    </Box>
+    </Container>
   );
 };
 
